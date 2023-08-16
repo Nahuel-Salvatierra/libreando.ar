b4ba86bf01ab2cc1486e85bdcc5e73bfac1ab528
Nahuel <131316681+Nahuel-Salvatierra@users.noreply.github.com>
2023-08-16 03:07:55 -0300

    feat: :sparkles: instagram API

--- src/components/instagramFeed/instagramFeed.jsx
+++ src/components/instagramFeed/instagramFeed.jsx
@@ -1,102 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 export function Instagram() {
     var url = "https://graph.instagram.com/me/media";
     var fields = "?fields=media_type,media_url,caption,permalink&access_token=";
-    var access_token =
-        "IGQWROSG5kRndCaEFBR3Uza1oxRnNRNHpzV1BsaGVwRTN4bjg1eTlLamVyLTZA3YmItUE92WWJwekdYV0lCdUoyZAlFaQ0RzRmtqLXpyQkFhWWhOVENDOTE1YlJIQ1JualVtYjFoVGJOWXVrNmhRQm9DOC0yU3V5RkEZD";
+    var access_token = 'IGQWRNNmNXOUV2S1JSeEZAyYXZAiNDVlZA0ZAnenNEamw1LVcybFJaRE5OSTY2WDBVbVNEd3YzRmYyNWRHYUlpUDRUOURBeV9wQ0draExDNDlTR0hZAOS1TMkhDS05EaW1OT3BVMTJST184WlZAKSkxjZATk3TzcxZAHgwS2sZD'
 
     var urlFetch = url + fields + access_token;
 
-    fetch(urlFetch)
-        .catch((error) => console.error(error))
-        .then((response) => response.json())
-        .then((arrayData) => function1(arrayData));
-
-
     const [data, setData] = useState([]);
-    userE
+    useEffect(() => {
+      const fetchData = async () => {
+          try {
+              const response = await fetch(urlFetch);
+              const responseData = await response.json()
+              setData(responseData.data)
+          } catch (error) {
+              console.error(error);
+          }
+      };
+  
+      fetchData();
+  }, []);
+  
 return (
-  <></>
-  let function1 = (arrayData) => {
-    let html = "";
-    arrayData.data.map((itemArray) => function2(itemArray));
-
-    function function2(itemArray2) {
-        let htmlDemo1 = "";
-        htmlDemo1 += `<img src="${itemArray2.media_url}">`;
-        htmlDemo1 += `
-      <a class="icon-instagram" href="${itemArray2.permalink}" target="_blank" rel="noopener noreferrer">
-      </a>
-      `;
-
-        if (itemArray2.media_type === "IMAGE") {
-            let item_caption = itemArray2.caption;
-            function funcCaption() {
-                // ========== url... ==========
-                if (item_caption) {
-                    const urlRegex = /(https?:\/\/[^\s]+)/g;
-                    // Find all matches
-                    const urlMatches = item_caption.match(urlRegex);
-
-                    if (urlMatches && urlMatches.length > 0) {
-                        htmlDemo1 += `
-                  <a class="icon-shopping-cart" href="${urlMatches[0]}" target="_blank" rel="noopener noreferrer">
-                  </a>
-                `;
-                    }
-
-                    //   console.log(urlMatches[2]);
-                    // ========== url. ==========
-                    // ========== hashtag... ==========
-
-                    // Regular expression to match hashtags
-                    const hashtagRegex = /#(\w+)/g;
-                    // Find all matches
-                    const hashMatches = item_caption.match(hashtagRegex);
-                    // Log each extracted hashtag separately
-                    hashMatches.forEach((match) => {
-                        const matchSlice = match.slice(1);
-                    });
-
-                    const lines = item_caption.split("\n");
-                    const title = lines[0];
-
-                    // Output: "Title rhinoceros"
-                    console.log(title);
-                    htmlDemo1 += `
-          <p class="title">${title}</P>
-          `;
-
-                    // ========== title. ==========
-                    // ========== content... ==========
-
-                    // Remove the link
-                    const linkRemoved = item_caption.replace(
-                        /https?:\/\/\S+/g,
-                        ""
-                    );
-
-                    // Remove the hashtag
-                    const hashtagRemoved = linkRemoved.replace(/#\S+/g, "");
-
-                    // console.log(hashtagRemoved);
-                    // ========== content. ==========
-                }
-            }
-            funcCaption();
-            // html += `
-            // <p>${item_caption}</p>
-            // `;
-            let aside = document.createElement("aside");
-            aside.innerHTML = htmlDemo1;
-            aside.classList.add("imgContainer"); // Add the class to the aside element
-            let mainContainer = document.querySelector(".mainContainer");
-            mainContainer.insertAdjacentHTML("beforeend", aside.outerHTML);
-        }
-    }
-    // mainContainer.insertAdjacentHTML("beforeend", html);
-};
-)
-    
+  <a className="row gap-3 mt-3 justify-content-center" href="">
+      {data.map((objeto) => (
+          <div className="card col-xl-3 col-md-5">
+              <img src={objeto.media_url} className="card-img-top" alt="..." />
+              <div className="card-body row align-items-end">
+                  <strong className="card-title">{objeto.nombre}</strong>
+                  <p className="card-text">
+                      Modelo: <br></br>
+                      {objeto.modelo}
+                  </p>
+                  <div
+                      href="#"
+                      className="btn btn-primary espacio-lateral"
+                  >
+                      <i class="bi bi-cart3"></i>Añadir
+                  </div>
+              </div>
+          </div>
+      ))}
+  </a>
+);
 }
