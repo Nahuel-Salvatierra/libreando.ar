6cefe737c36816259b9d8589760ec53c7a6afe1d
Gonzalez-Ignacio <ignaciogonzalez2306@gmail.com>
2023-08-17 02:59:15 -0300

    feat: :sparkles: Comp Red Social

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 // Ruta Nav
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
-import { Instagram } from './components/instagramFeed/instagramFeed';
+import BotonRedesSociales from './components/BotonRedesSociales';
 // Component
+// import { Instagram } from './components/instagramFeed/instagramFeed';
 
 // Paginas
 
@@ -39,13 +40,14 @@ function App() {
             {/* Talleres */}
             {/* <Route path="/talleres" element={<Talleres />} /> */}
           </Routes>
-          <Instagram/>
+          {/* <Instagram/> */}
 
         </main>
 
         <footer>
           {/* Footer */}
           {/* <Footer /> */}
+          <BotonRedesSociales />
         </footer>
 
       </Router>
