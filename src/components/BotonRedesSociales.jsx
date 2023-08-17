067925c0897470f9ee65021c864e15e3dee05d3d
Gonzalez-Ignacio <ignaciogonzalez2306@gmail.com>
2023-08-17 04:24:07 -0300

    feat: :sparkles: Estilos Redes Sociales
    he realizado cambios en los estilos de las Redes Sociales

--- src/components/BotonRedesSociales.jsx
+++ src/components/BotonRedesSociales.jsx
@@ -1,37 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 function BotonRedesSociales() {
-    const [showRedes, setShowRedes] = useState(true);
+  const [showRedes, setShowRedes] = useState(false);
 
-    useEffect(() => {
-        const btnMensaje = document.querySelector('.btn-mensaje');
+  const toggleRedes = () => {
+    setShowRedes(!showRedes);
+  };
 
-        const btnToggle = () => {
-            setShowRedes(!showRedes);
-        };
-
-        btnMensaje.addEventListener('click', btnToggle);
-
-        return () => {
-            btnMensaje.removeEventListener('click', btnToggle);
-        };
-    }, [showRedes]);
-
-    return (
-        <div className='container-icon'>
-            <div className={`redes ${showRedes ? 'show' : 'hide'}`}>
-                <a href="https://wa.me/541165776749" target="_blank" className='icono-whatsapp'>
-                    <i className="bi bi-whatsapp"></i>
-                </a>
-                <a href="https://www.instagram.com/n4h92" target="_blank">
-                    <i className="bi bi-instagram icono-instagram"></i>
-                </a>
-            </div>
-            <div className="btn-mensaje text-white">
-                <i className="bi bi-chat-dots-fill icon-mensaje"></i>
-            </div>
-        </div>
-    );
+  return (
+    <div className='container-icon'>
+      <div className={`redes ${showRedes ? 'show' : 'hide'}`}>
+        <a href="https://wa.me/541165776749" target="_blank" className='icono-whatsapp'>
+          <i className="bi bi-whatsapp "></i>
+        </a>
+        <a href="https://www.instagram.com/libreando" target="_blank" className=''>
+          <i className="bi bi-instagram icono-instagram"></i>
+        </a>
+      </div>
+      <div className="btn-mensaje" id='btn-mensaje' onClick={toggleRedes}>
+        <i className="bi bi-chat-dots-fill  icon-mensaje"></i>
+      </div>
+    </div>
+  );
 }
 
 export default BotonRedesSociales;
