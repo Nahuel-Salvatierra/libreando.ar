ff21c59afa233686475cef4a00ceac543a3090fa
Nahuel <131316681+Nahuel-Salvatierra@users.noreply.github.com>
2023-08-24 23:35:22 -0300

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import 'dotenv/config'
 
 // https://vitejs.dev/config/
-
-export default()=>{
-  if (process.env.VITE_REACT_APP_DEVELOPMENT=='true') {
-    return defineConfig({
-      plugins: [react()]
-    })
-  } else {
-    return defineConfig({
-      plugins: [react()],
-      base: '/libreando.ar/'
-    })
-  }
-}
-
-
-
+export default defineConfig({
+  plugins: [react()],
+  base:'/libreando.ar/'
+})
