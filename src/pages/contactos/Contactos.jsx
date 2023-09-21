298978bdc16784cd3aae9ccabef3f13597ba3fdb
Gonzalez-Ignacio <ignaciogonzalez2306@gmail.com>
2023-09-20 21:05:21 -0300

    feat: :sparkles: Pagina Contacto
    Hemos añadido un formulario pedido por el cliente

---
+++ src/pages/contactos/Contactos.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Contactos() {
+    return (
+        <div>
+            <section className='container'>
+                {/* Formulario */}
+                <div className='text-center'>
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdgG6E6CFXQZIVP732-g_gWV8Peu08XkOPi0UEYBkeip6cN9A/viewform?embedded=true" width="640" height="600" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Contactos
