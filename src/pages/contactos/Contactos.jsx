ec2fd54a90a27fdd318e2642b9cf5c9d836e5805
Gonzalez-Ignacio <ignaciogonzalez2306@gmail.com>
2023-09-27 18:22:26 -0300

    style: :art: Cambio de visualizacion en la Card de contactos.
    eliminamos el icono de facebook, agregamos el whatsapp y agregamos los links correspondientes a su icono/texto

--- src/pages/contactos/Contactos.jsx
+++ src/pages/contactos/Contactos.jsx
@@ -4,7 +4,7 @@ import "./Contacto.css"
 function Contactos() {
     return (
         <>
-            <section className='container'>
+            <section className='container '>
                 {/* Formulario */}
 
                 <div className="row">
@@ -14,30 +14,34 @@ function Contactos() {
                             <div className="card-body p-0 p-xl-4 py-lg-5">
                                 <div className="d-flex caja-contenido align-items-center justify-content-center">
                                     <div className="text-center">
-                                        <div className="h6 fw-bolder">
-                                            ¿Tienes más dudas?
-                                        </div>
                                         <p className="text-muted mb-4">
                                             Contactanos
                                             <br />
-                                            <a href="#!">
-                                                equipolibreando@gmail.com
-                                            </a>
+                                            <div>
+
+                                                <a href="#!">
+                                                    equipolibreando@gmail.com
+                                                </a>
+                                            </div>
+                                            <div>
+                                                <a
+                                                    className="fs-5 px-2 link-dark text-decoration-none"
+                                                    href="https://wa.me/541139004966"
+                                                >
+                                                    <i className="bi-whatsapp"></i>
+                                                    &nbsp;541139004966
+                                                </a>
+                                            </div>
                                         </p>
                                         <div className="h6 fw-bolder">
                                             Síguenos!
                                         </div>
                                         <a
-                                            className="fs-5 px-2 link-dark"
-                                            href="https://www.facebook.com/people/NNS/61550064306296/"
-                                        >
-                                            <i className="bi-facebook"></i>
-                                        </a>
-                                        <a
-                                            className="fs-5 px-2 link-dark"
-                                            href="https://www.instagram.com/nns.digital/"
+                                            className="fs-5 px-2 link-dark text-decoration-none"
+                                            href="https://www.instagram.com/equipolibreando/"
                                         >
                                             <i className="bi-instagram"></i>
+                                            &nbsp;equipolibreando
                                         </a>
 
                                     </div>
